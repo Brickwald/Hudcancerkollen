3c0bc5d4cea1b34ad1c3cf29ef897b0d4a2a3284
Brickwald <falk-rickard@hotmail.com>
2023-02-05 19:06:07 +0100

    Change Top result text

--- Solstudie/level1.tsx
+++ Solstudie/level1.tsx
@@ -9,6 +9,12 @@ export default class Level1 extends React.Component {
 
     render()
     {
+        return (
+            <div>
+                <p>Eftersom det här är en testversion av Hudcancerkollen får du i den versionen ingen återkoppling kring hur din sammanvägda hudcancerrisk ser ut i jämförelse med andra, utan enbart vilka specifika riskfaktorer du har.</p>
+            </div>
+        );
+        /* Detta visar olika beroende på risk
         if (this.props.risk < 132)
         {
             return (
@@ -41,7 +47,7 @@ export default class Level1 extends React.Component {
                 </div>
             );
         }
-
+        */
     }
 
 }
